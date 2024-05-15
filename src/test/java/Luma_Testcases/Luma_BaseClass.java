0a418f2a4ef8457e0ae46ae5b4eeda2de37a1f79
kirandarapu <naveendarapu108@gmail.com>
2024-05-15 15:27:43 +0530

    Login Functionality Task source code

--- src/test/java/Luma_Testcases/Luma_BaseClass.java
+++ src/test/java/Luma_Testcases/Luma_BaseClass.java
@@ -1,5 +1,23 @@
 package Luma_Testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
 public class Luma_BaseClass {
+	
+	public static WebDriver driver;
+	@BeforeClass
+	public void OpenApllication() {
+		
+		driver=new ChromeDriver();
+		
+		driver.get("https://magento.softwaretestingboard.com/customer/account/login/");
+	}
+	
+	@AfterClass
+	public void CloseApplication() {
+		driver.close();
+	}
 
 }
